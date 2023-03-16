5cda3b4f15ac950f10f8ad1be5052a43823c3b67
Joao Afonso <ferreira.afonso.joao002@gmail.com>
2023-03-16 15:46:19 +0000

    update

---
+++ Aula04/Rent-a-car ex_4_1_1.sql
@@ -0,0 +1,38 @@
+﻿CREATE TABLE RentACar_Balcao (
+	[Numero] [int] NOT NULL PRIMARY KEY,
+	[Nome] [varchar](256)NOT NULL,
+	[Endereco] [varchar](1024)NOT NULL,
+)
+GO
+
+CREATE TABLE RentACar_Cliente (
+  	[Nif] [int] NOT NULL PRIMARY KEY,
+  	[Nome] [varchar](256) NOT NULL,
+  	[Endereco] [varchar](1024)NOT NULL,
+  	[num_carta] [varchar](32 )NOT NULL,
+)
+GO
+
+CREATE TABLE RentACar_Ligeiro (
+ 	[Codigo] [int] NOT NULL PRIMARY KEY,
+	[Arcondicionado] [varchar](256) NOT NULL,
+	[Designacao] [varchar](256) NOT NULL,
+	[NumLugares] [int] NOT NULL,
+	[Portas][int] NOT NULL,
+	[Combustivel][varchar](256) NOT NULL
+)
+GO
+
+CREATE TABLE RentACar_Pesado (
+ 	[Codigo] [int] NOT NULL PRIMARY KEY,
+	[Arcondicionado] [varchar](256) NOT NULL,
+	[Designacao] [varchar](256) NOT NULL,
+	[Peso] [int] NOT NULL,
+	[Passageiros] [int] NOT NULL
+)
+GO
+
+
+
+
+
