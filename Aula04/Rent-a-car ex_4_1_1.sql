e75bf730ea6d5108d52b7deced959eed5b3b5ce6
Joao Afonso <ferreira.afonso.joao002@gmail.com>
2023-03-16 16:03:27 +0000

    «

--- Aula04/Rent-a-car ex_4_1_1.sql
+++ Aula04/Rent-a-car ex_4_1_1.sql
@@ -14,9 +14,7 @@ CREATE TABLE RentACar_Cliente (
 GO
 
 CREATE TABLE RentACar_Ligeiro (
- 	[Codigo] [int] NOT NULL PRIMARY KEY,
-	[Arcondicionado] [varchar](256) NOT NULL,
-	[Designacao] [varchar](256) NOT NULL,
+ 	[Codigo] [int] NOT NULL PRIMARY KEY REFERENCES RentACar_Tipo_Veiculo([codigo]),
 	[NumLugares] [int] NOT NULL,
 	[Portas][int] NOT NULL,
 	[Combustivel][varchar](256) NOT NULL
@@ -24,13 +22,43 @@ CREATE TABLE RentACar_Ligeiro (
 GO
 
 CREATE TABLE RentACar_Pesado (
- 	[Codigo] [int] NOT NULL PRIMARY KEY,
-	[Arcondicionado] [varchar](256) NOT NULL,
-	[Designacao] [varchar](256) NOT NULL,
+ 	[Codigo] [int] NOT NULL PRIMARY KEY REFERENCES RentACar_Tipo_Veiculo([codigo]),
 	[Peso] [int] NOT NULL,
 	[Passageiros] [int] NOT NULL
 )
 GO
+
+CREATE TABLE RentACar_Tipo_Veiculo (
+	[Codigo] [int] NOT NULL IDENTITY PRIMARY KEY,
+	[Designacao] [varchar](256) NOT NULL,
+	[ArCondicionado] [bit] NOT NULL
+)
+GO
+
+CREATE TABLE RentACar_Veiculo (
+ 	[Matricula] [varchar](16) NOT NULL PRIMARY KEY,
+	[Ano] [int] NOT NULL,
+	[Marca] [varchar](256) NOT NULL,
+	[Tipo_Veiculo_Codigo] [int] NOT NULL REFERENCES RentACar_Tipo_Veiculo([codigo])
+)
+GO
+
+CREATE TABLE RentACar_Aluguer (
+ 	[Matricula] [varchar](16) NOT NULL PRIMARY KEY,
+	[Ano] [int] NOT NULL,
+	[Marca] [varchar](256) NOT NULL,
+	[Tipo_Veiculo_Codigo] [int] NOT NULL REFERENCES RentACar_Tipo_Veiculo([codigo]),
+	[Cliente_Nif] [int] NOT NULL REFERENCES RentACar_Cliente([Nif]),
+	[Balcao_Numero] [int] NOT NULL REFERENCES RentACar_Balcao([Numero]),
+)
+GO
+
+CREATE TABLE RentACar_Similaridade (
+	[Codigo1] [int] NOT NULL REFERENCES RentACar_Tipo_Veiculo([Numero]),
+	[Codigo2] [int] NOT NULL REFERENCES RentACar_Tipo_Veiculo([Numero]),
+)
+GO
+
 
 
 
